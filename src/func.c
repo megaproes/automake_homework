741fa4407f6a2ab6368f7f86adb63760f02e9705
alex <sasha.borchenko@gmail.com>
2025-10-07 15:24:43 +0300

    Add function definitions and Makefile

---
+++ src/func.c
@@ -0,0 +1,5 @@
+#include "myfunc.h"
+
+int add(int a, int b) { return a + b; }
+
+const char *greet(void) { return "Hello from myfunc.h!"; }
---
+++ src/main.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include "myfunc.h"
+
+int main(void) { return printf("%s\n2 + 3 = %d\n", greet(), add(2, 3)); }
